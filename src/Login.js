7f4c1ba3a4bc18d673d68a0e4076dcb8e5253a88
Souhail Hoichtia <souhailsrp@gmail.com>
2021-11-30 00:05:09 +0100

    first commit+++ adding first componenet: Login componenent

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import "./Login.css";
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/5/56/Spotify_logo_horizontal_black.jpg"
+        alt=""
+      ></img>
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
